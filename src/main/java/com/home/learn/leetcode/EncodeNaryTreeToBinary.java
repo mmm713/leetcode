a621f6e84654da424b2fc098f44e42a976ddc7b8
mmm713 <mmm713@users.noreply.github.com>
2020-03-01 23:40:48 -0800

    Update for uber
    done

---
+++ src/main/java/com/home/learn/leetcode/EncodeNaryTreeToBinary.java
@@ -0,0 +1,66 @@
+package com.home.learn.leetcode;
+
+import com.home.learn.library.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EncodeNaryTreeToBinary {
+
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+    // Encodes an n-ary tree to a binary tree.
+    public TreeNode encode(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode newRoot = new TreeNode(root.val);
+
+        // Encode the first child of n-ary node to the left node of binary tree.
+        if (root.children.size() > 0) {
+            Node firstChild = root.children.get(0);
+            newRoot.left = this.encode(firstChild);
+        }
+
+        // Encoding the rest of the sibling nodes.
+        TreeNode sibling = newRoot.left;
+        for (int i = 1; i < root.children.size(); ++i) {
+            sibling.right = this.encode(root.children.get(i));
+            sibling = sibling.right;
+        }
+
+        return newRoot;
+    }
+
+    // Decodes your binary tree to an n-ary tree.
+    public Node decode(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Node newRoot = new Node(root.val, new ArrayList<>());
+
+        // Decoding all the children nodes
+        TreeNode sibling = root.left;
+        while (sibling != null) {
+            newRoot.children.add(this.decode(sibling));
+            sibling = sibling.right;
+        }
+
+        return newRoot;
+    }
+}
--- src/main/java/com/home/learn/library/TreeNode.java
+++ src/main/java/com/home/learn/library/TreeNode.java
@@ -1,8 +1,8 @@
 package com.home.learn.library;
 
 public class TreeNode {
-     int val;
+     public int val;
      public TreeNode left;
      public TreeNode right;
-     TreeNode(int x) { val = x; }
+     public TreeNode(int x) { val = x; }
 }
