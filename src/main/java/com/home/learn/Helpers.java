670b5af00593537ca15dc76afa516f31b00bd74f
weiwang-doordash <wei.wang@doordash.com>
2022-09-07 14:12:47 -0700

    add tree print

--- src/main/java/com/home/learn/Helpers.java
+++ src/main/java/com/home/learn/Helpers.java
@@ -1,6 +1,10 @@
 package com.home.learn;
 
+import com.home.learn.library.TreeNode;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Helpers {
@@ -27,5 +31,88 @@ public class Helpers {
 
     public static void print1D(String[] mat) {
         System.out.println(Arrays.toString(mat));
+    }
+
+    static class TreePrinter {
+        public static void printNode(TreeNode root) {
+            int maxLevel = TreePrinter.maxLevel(root);
+
+            printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+        }
+
+        private static void printNodeInternal(List<TreeNode> nodes, int level, int maxLevel) {
+            if (nodes.isEmpty() || TreePrinter.isAllElementsNull(nodes))
+                return;
+
+            int floor = maxLevel - level;
+            int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+            int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+            int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+            TreePrinter.printWhitespaces(firstSpaces);
+
+            List<TreeNode> newNodes = new ArrayList<>();
+            for (TreeNode node : nodes) {
+                if (node != null) {
+                    System.out.print(node.val);
+                    newNodes.add(node.left);
+                    newNodes.add(node.right);
+                } else {
+                    newNodes.add(null);
+                    newNodes.add(null);
+                    System.out.print(" ");
+                }
+
+                TreePrinter.printWhitespaces(betweenSpaces);
+            }
+            System.out.println();
+
+            for (int i = 1; i <= endgeLines; i++) {
+                for (TreeNode node : nodes) {
+                    TreePrinter.printWhitespaces(firstSpaces - i);
+                    if (node == null) {
+                        TreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+                        continue;
+                    }
+                    if (node.left != null)
+                        System.out.print("/");
+                    else
+                        TreePrinter.printWhitespaces(1);
+
+                    TreePrinter.printWhitespaces(i + i - 1);
+
+                    if (node.right != null)
+                        System.out.print("\\");
+                    else
+                        TreePrinter.printWhitespaces(1);
+
+                    TreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+                }
+
+                System.out.println("");
+            }
+
+            printNodeInternal(newNodes, level + 1, maxLevel);
+        }
+
+        private static void printWhitespaces(int count) {
+            for (int i = 0; i < count; i++)
+                System.out.print(" ");
+        }
+
+        private static int maxLevel(TreeNode node) {
+            if (node == null)
+                return 0;
+
+            return Math.max(TreePrinter.maxLevel(node.left), TreePrinter.maxLevel(node.right)) + 1;
+        }
+
+        private static <T> boolean isAllElementsNull(List<T> list) {
+            for (Object object : list) {
+                if (object != null)
+                    return false;
+            }
+            return true;
+        }
     }
 }
--- src/main/java/com/home/learn/MainClass.java
+++ src/main/java/com/home/learn/MainClass.java
@@ -1,12 +1,29 @@
 package com.home.learn;
 
+import com.home.learn.google.DeleteNodesAndReturnForest;
 import com.home.learn.library.TreeNode;
 
 import java.util.*;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        Set<Integer> toDel = new HashSet<>(2,7);
+        TreeNode root = new TreeNode(0);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        root.left = node1;
+        node1.left = node2;
+        node1.right = node3;
+        root.right = node4;
+        node4.left = node5;
+        node2.right = node6;
+        Helpers.TreePrinter.printNode(root);
+        int[] toDel = {1, 5};
+        DeleteNodesAndReturnForest test = new DeleteNodesAndReturnForest();
+        List<TreeNode> res = test.delNodes(root, toDel);
+        res.forEach(Helpers.TreePrinter::printNode);
     }
 }
